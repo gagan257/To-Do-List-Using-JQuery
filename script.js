329fda1a4124a8ae31e30efdf077bad3928065d9
gagan257 <arorag2577@gmail.com>
2023-01-13 13:28:15 +0530

    Todo-list-using-Jquery-JS

--- script.js
+++ script.js
@@ -46,3 +46,47 @@ btnReset.click(() => {
 })
 btnCleanup.click(clearDone)
 btnSort.click(sortTasks)
+
+//****************Extra Code********************
+
+// function addItem() {
+//     let listItem = $('<li>', {
+//         'class': 'list-group-item',
+//         text: inpNewTask.val()
+//     })
+//     listItem.click(() => {
+//         listItem.toggleClass('done')
+//     })
+//     ulTasks.append(listItem)
+//     inpNewTask.val('')
+//     toggleInputButtons()
+// }
+
+// function clearDone() {
+//     $('#ulTasks .done').remove()
+//     toggleInputButtons()
+// }
+
+// function sortTasks() {
+//     $('#ulTasks .done').appendTo(ulTasks)
+// }
+
+// function toggleInputButtons() {
+//     btnReset.prop('disabled', inpNewTask.val() == '')
+//     btnAdd.prop('disabled', inpNewTask.val() == '')
+//     btnSort.prop('disabled', ulTasks.children().length < 1)
+//     btnCleanup.prop('disabled', ulTasks.children().length < 1)
+// }
+
+// inpNewTask.keypress((e) => {
+//     if (e.which == 13) addItem()
+// })
+// inpNewTask.on('input', toggleInputButtons)
+
+// btnAdd.click(addItem)
+// btnReset.click(() => {
+//     inpNewTask.val('')
+//     toggleInputButtons()
+// })
+// btnCleanup.click(clearDone)
+// btnSort.click(sortTasks)
